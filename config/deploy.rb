05de49e0dba7277d76eeb628f457cb4c906013ef
Danil Pismenny <danil@orionet.ru>
2011-02-06 21:49:16 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,10 @@ namespace :vlad do
     Rake::Task['vlad:bundle'].invoke
   end
 
+  remote_task :setup do
+    Rake::Task['vlad:update_gems'].invoke
+  end
+
   #
   # Fixes vlad/passenger bad latest_release path
   #
