6dd91aba017a0ac03cfede03dc069c33adfbc6eb
Danil Pismenny <danil@orionet.ru>
2011-02-13 00:08:17 +0300

    * fixed bug в коде площадки

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,5 +1,5 @@
 module PlacesHelper
   def generate_place_code(place)
-    "<div class='aydabanner' id='aydabanner_#{place.id}' style='width: #{place.format.width}; height: #{place.format.height}'/>"
+    "<div class='aydabanner' id='aydabanner_#{place.id}' style='width: #{place.format.width}px; height: #{place.format.height}px'/>"
   end
 end
